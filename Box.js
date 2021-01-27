e24f99327ca6ed4067f60819990e7c4945650105
KingAgastya <63898735+KingAgastya@users.noreply.github.com>
2021-01-27 12:35:31 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,18 @@
+class Box {
+    constructor(x, y, width, height) {
+        var options = {
+            isStatic : false,
+            density : 0.001,
+            friction : 1
+        }
+        this.visibility = 255;
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+    }
+    display(color, border) {
+        var pos = this.body.position;
+        var angle = this.angle;   
+    }
+}
